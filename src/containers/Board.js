4b96e9b1f6f567b108b31c13b34494f2d2ab2954
Darkoson <darkothomas80@gmail.com>
2022-02-17 23:54:26 +0000

    creation of Ticket

--- src/containers/Board.js
+++ src/containers/Board.js
@@ -14,7 +14,38 @@ const BoardWrapper = styled.div`
 `;
 
 class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+      error: "",
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const tickets = await fetch('../../assets/data.json');
+      const ticketsJson = await tickets.json();
+      //  console.log("ticketsJson=", ticketsJson);
+
+      if (ticketsJson) {
+        // console.log("ticketsJson=", ticketsJson);
+        this.setState({
+          data: ticketsJson,
+          loading: false,
+        });
+      }
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error.message,
+      });
+    }
+  }
+
   render() {
+    const { data, loading, error } = this.state;
     const lanes = [
       { id: 1, title: "To Do" },
       { id: 2, title: "In Progress" },
@@ -25,7 +56,13 @@ class Board extends Component {
     return (
       <BoardWrapper>
         {lanes.map((lane) => (
-          <Lane key={lane.id} title={lane.title} />
+          <Lane
+            key={lane.id}
+            title={lane.title}
+            loading={loading}
+            error={error}
+            tickets={data.filter((ticket) => ticket.lane === lane.id)}
+          />
         ))}
       </BoardWrapper>
     );
